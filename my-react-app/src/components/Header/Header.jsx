6b2a23eab3a255222c2f2f153b79c55a62336cbf
RickyMarthin <rarp01misterius@gmail.com>
2023-12-26 22:49:04 -0500

    Se logro agregar el Navbar

--- my-react-app/src/components/Header/Header.jsx
+++ my-react-app/src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
+import SearchForm from "../SearchForm/SearchForm";
+import "./Header.css"
 
 const Header = () => {
     return (
-        <div>
-
+        <div className="holder">
+            <header className="header">
+                <Navbar/>
+                <div className="header-content flex flex-center text-center text-white">
+                    <h2 className="header-title text-capitalize">find your book of choice.
+                    </h2><br />
+                    <p className="header-text fs-18 fw-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium a totam aspernatur iste laborum veritatis, placeat debitis vitae ipsam non facere quasi. In voluptates deserunt totam, ipsam ipsa veniam!
+                    </p>
+                    <SearchForm/>
+                </div>
+            </header>
         </div>
     )
 }
