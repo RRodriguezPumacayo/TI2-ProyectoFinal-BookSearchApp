a2dca02af7d44304679c9d3a7cccbf2374204f86
ANTHONYCCOLQUE <104391632+ANTHONYCCOLQUE@users.noreply.github.com>
2023-12-27 21:00:27 -0500

    Update Header.jsx

--- my-react-app/src/components/Header/Header.jsx
+++ my-react-app/src/components/Header/Header.jsx
@@ -1,23 +1,21 @@
-import React from "react";
+import React from 'react';
 import Navbar from "../Navbar/Navbar";
 import SearchForm from "../SearchForm/SearchForm";
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div className="holder">
-            <header className="header">
-                <Navbar/>
-                <div className="header-content flex flex-center text-center text-white">
-                    <h2 className="header-title text-capitalize">find your book of choice.
-                    </h2><br />
-                    <p className="header-text fs-18 fw-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium a totam aspernatur iste laborum veritatis, placeat debitis vitae ipsam non facere quasi. In voluptates deserunt totam, ipsam ipsa veniam!
-                    </p>
-                    <SearchForm/>
-                </div>
-            </header>
-        </div>
-    )
+  return (
+    <div className='holder'>
+        <header className='header'>
+            <Navbar />
+            <div className='header-content flex flex-c text-center text-white'>
+                <h2 className='header-title text-capitalize'>Search Books</h2><br />
+                <p className='header-text fs-18 fw-3'></p>
+                <SearchForm />
+            </div>
+        </header>
+    </div>
+  )
 }
 
 export default Header
